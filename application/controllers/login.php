ee4d3405ab35c086229b23ad25f672ff1231d1e5
acqua01 <acqua.dev@hushmail.com>
2014-11-02 09:31:09 +0530

    login.php
    login control

---
+++ application/controllers/login.php
@@ -0,0 +1,37 @@
+<?php
+class Login extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('template');
+        $this->load->helper('url');
+        
+    }
+
+    public function first_page($msg = NULL)
+    {
+   	$this->template->title('Login');
+       // $this->load->view('login');
+		 $data['msg'] = $msg;
+        $this->load->view('login',$data);
+		 }
+	public function process()
+	{
+	
+		$this->load->model('login_model');
+		$result=$this->login_model->validate();
+		if(!$result)
+		{
+		$msg = '<font color=red>Invalid username and/or password.</font><br />';
+			$this->first_page($msg);
+		}
+		else
+		{
+			redirect("welcome/home");
+		}
+	}
+		
+    
+}
+?>
